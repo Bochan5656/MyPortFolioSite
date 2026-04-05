baae13f5e52bf75b1f61df1ba77ed3d86f08bd6a
bochan <ishimarur0621@gmail.com>
2026-04-05 14:34:26 +0900

    first commit

---
+++ wp-content/themes/MyPortfolio2025/assets/js/main.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+  $('a').css({
+    'font-family': 'Cardo',
+    'font-style': 'italic',
+    'color': '#f8f5f3'
+  });
+  $('.title').css({
+    'font-family': 'Cardo',
+    'font-style': 'italic',
+  });
+  $('h2').css({
+    'font-family': 'cardo',
+    'font-style': 'italic',
+    'font-weight': 'normal',
+  });
+  $('h3').css({
+    'font-family': 'cardo',
+    'font-style': 'italic',
+    'font-weight': 'normal',
+  });
+  $('p').css({
+    'font-family': 'kaisei Opti',
+    'font-weight': 'normal',
+  });
+  $('button').css({
+    'font-family': 'cardo',
+    'font-style': 'normal',
+    'font-weight': 'normal',
+  });
+});
